edbf6109a0d974bd91e54ac5018d50aebab75200
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-02-03 08:55:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ function App() {
   return (
     <div className="App">
     <h3>Main Branch</h3>
-    <p>This Repo was made using a template made from a clean React bootstrap</p>
-    <p>The majority of the default react bootstrap boilerplate has been removed</p>
     </div>
   );
 }
