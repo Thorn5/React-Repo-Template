51d9ba9deb7c584c209a09a01802321223ee24d2
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-02-03 10:21:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
     <h3>Main Branch</h3>
+    <p>This app was created using a clean bootstrapped react template.</p>
+    <p>The stock react App.js code has been removed.</p>
     </div>
   );
 }
